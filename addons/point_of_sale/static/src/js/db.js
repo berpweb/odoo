c5f92228145189f8f9efc98233e9f9dbed6082f5
BusinessERPWeb <businesserpweb@gmail.com>
2016-12-23 23:37:04 +0530

    bk_custom - increase limit of products on pos

--- addons/point_of_sale/static/src/js/db.js
+++ addons/point_of_sale/static/src/js/db.js
@@ -7,7 +7,7 @@ function openerp_pos_db(instance, module){
 
     module.PosDB = instance.web.Class.extend({
         name: 'openerp_pos_db', //the prefix of the localstorage data
-        limit: 100,  // the maximum number of results returned by a search
+        limit: 10000,  // the maximum number of results returned by a search
         init: function(options){
             options = options || {};
             this.name = options.name || this.name;
